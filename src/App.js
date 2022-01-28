9860d1bdf1692147ffe73a0c4cefa85d503bda4b
Vinit Hemadri <vineet.vh@gmail.com>
2022-01-28 21:13:31 +0530

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layouts/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl">Hello World!</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
