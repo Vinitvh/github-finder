885063537322803398ae21f3059484125bb7acb0
Vinit Hemadri <vineet.vh@gmail.com>
2022-02-07 11:16:58 +0530

    Added about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React App to search for GitHub profiles and see profile details.
+      </p>
+    </>
+  );
 }
 
 export default About;
